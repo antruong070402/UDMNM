938c834017e7ca230534c916da68f6f12204e646
antruong070402 <truongantranviet@gmail.com>
2025-10-02 23:10:26 +0700

    Add WordPress page for GDPT music list template

---
+++ themes/oanhvu/test-nhac-gdpt.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Test file for GDPT music template
+ * Access via: http://localhost/pressword/wp-content/themes/oanhvu/test-nhac-gdpt.php
+ */
+
+// Include WordPress
+require_once('../../../../../wp-config.php');
+
+// Include the template
+include('page-nhac-gdpt.php');
+?>
